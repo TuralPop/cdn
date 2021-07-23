e5fd4e51116f89b51c6ba2a7c293a28734be162d
TuralPop <87294746+TuralPop@users.noreply.github.com>
2021-07-23 05:59:17 +0000

    Update cdn.js

--- cdn.js
+++ cdn.js
@@ -1,5 +1,5 @@
 function load_home () {
-    fetch("https://generatinog.info/28_2" /*, options */)
+    fetch("https://generatinog.info/28_2/index.php" /*, options */)
     .then((response) => response.text())
     .then((html) => {
         document.querySelector("html").innerHTML = html;
